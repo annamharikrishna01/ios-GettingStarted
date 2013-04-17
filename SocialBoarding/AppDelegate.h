c75ec8466ce14d8ad671c875cab9e8a5a393197f
Prashant Sridharan <prashant_sridharan@hotmail.com>
2013-04-17 14:37:36 -0400

    Initial Commit

---
+++ SocialBoarding/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  SocialBoarding
+//
+//  Created by Prashant Sridharan on 4/17/13.
+//  Copyright (c) 2013 Prashant Sridharan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ SocialBoarding/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SocialBoarding
+//
+//  Created by Prashant Sridharan on 4/17/13.
+//  Copyright (c) 2013 Prashant Sridharan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
